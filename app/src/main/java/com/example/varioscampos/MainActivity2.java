bd028a3ca3edbb3138aec25841bab05e2ff1b687
Francisco Marques <francisco_marques00@hotmail.com>
2020-04-07 16:32:04 +0100

    2 codigo

--- app/src/main/java/com/example/varioscampos/MainActivity2.java
+++ app/src/main/java/com/example/varioscampos/MainActivity2.java
@@ -2,7 +2,9 @@ package com.example.varioscampos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+
+        String Nome = intent.getStringExtra(MainActivity.EXTRA_NOME);
+        String Telemovel = intent.getStringExtra(MainActivity.EXTRA_TELEMOVEL);
+        String Email = intent.getStringExtra(MainActivity.EXTRA_EMAIL);
+        Integer Idade = intent.getIntExtra(MainActivity.EXTRA_IDADE,0);
+        Float Peso = intent.getFloatExtra(MainActivity.EXTRA_PESO,0);
+        Float Altura = intent.getFloatExtra(MainActivity.EXTRA_ALTURA,0);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textViewNome = findViewById(R.id.textViewNome);
+        TextView textViewTelemovel = findViewById(R.id.textViewTelefone);
+        TextView textViewEmail = findViewById(R.id.textViewEmail);
+        TextView textViewIdade = findViewById(R.id.textViewIdade);
+        TextView textViewPeso = findViewById(R.id.textViewPeso);
+        TextView textViewAltura = findViewById(R.id.textViewAltura);
     }
 }
